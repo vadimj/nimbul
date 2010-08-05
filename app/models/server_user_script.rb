60a03b133005702baa73c5bd205a3fa417ee5ceb
Carl P. Corliss <rabbitt@gmail.com>
2010-08-04 22:20:15 -0400

    Make server-control script handle the different types of volumes at startup correctly
    the server-control script now handles the three types of volume mount types:
      RestoreLatestSnapshot - create and use volume based on latest snapshot of the specified volume
      RestoreSnapshot - create and use volume based on a specific snapshot
      MountVolume - use a specific volume

--- app/models/server_user_script.rb
+++ app/models/server_user_script.rb
@@ -31,11 +31,44 @@ class ServerUserScript
     ].compact.first
   end
   
+  def available_zones
+    account.zones.collect{|z| z.name }
+  end
+  
+  def zone
+    server.default_resource_bundle.zone.try(:name) rescue available_zones.sort_by(&:rand).first
+  end
+  
   def volumes
-    server.volumes.collect { |a| [ a.cloud_resource.cloud_id, a.mount_point] }.flatten
+    volumes = server.default_resource_bundle.server_resources.select { |sr| [ServerSnapshot, ServerVolume].include? sr.class } rescue []
+    volumes.inject({}) do |hash,volume|
+      hash[volume.mount_point] = {
+        :name    => volume.cloud_resource.name,
+        :zone    => volume.cloud_resource.zone.try(:name) || self.zone || available_zones.sort_by(&:rand).first
+      }
+      type_data = case volume.mount_type
+        when 'RestoreLatestSnapshotMountType'
+          snaps = CloudSnapshot.find_all_by_provider_account_id_and_parent_cloud_id(account[:id], volume.cloud_resource.cloud_id)
+          latest_snapshot = snaps.sort!{ |a,b| b.start_time <=> a.start_time }.first
+          { :type => :snapshot, :id => (latest_snapshot.cloud_id rescue nil) }
+        when 'RestoreSnapshotMountType'
+          { :type => :snapshot, :id => (volume.cloud_resource.cloud_id rescue nil)}
+        when 'MountVolumeMountType'
+          { :type => :volume, :id => (volume.cloud_resource.cloud_id rescue nil)}
+      else
+        {}
+      end
+      
+      hash[volume.mount_point].merge!(type_data) unless type_data.empty?
+      hash
+    end
   end
   
   def addresses
-    server.addresses.collect { |a| a.cloud_resource.cloud_id }
+    addresses = server.default_resource_bundle.server_resources.select { |sr| sr.kind_of? ServerAddress } rescue []
+    addresses.inject({}) do |hash,address|
+      hash[address.cloud_resource.cloud_id] = address.cloud_resource.name
+      hash
+    end
   end
 end
