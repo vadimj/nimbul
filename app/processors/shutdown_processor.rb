882c3bdc6c1e1847eb9ed4038c359fdb78f662ba
Carl P. Corliss <rabbitt@gmail.com>
2010-08-19 11:01:13 -0400

    set is_ready to false if emissary sends a shutdown message

--- app/processors/shutdown_processor.rb
+++ app/processors/shutdown_processor.rb
@@ -2,7 +2,11 @@ class ShutdownProcessor < ApplicationProcessor
 
   subscribes_to :shutdown, :routing_key => 'shutdown.#'
 
-  def on_message(message)
-    logger.debug "ShutdownProcessor received: " + message
+  def on_message(msg)
+    server_id, cluster_id, account_id, cloud_id = message.args
+    
+    instance = Instance.find_by_instance_id_and_server_id(instance_id, server_id)
+    return unless instance and instance.running? and instance.ready?
+    instance.disable!
   end
 end
