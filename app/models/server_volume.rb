9fdca01428c997c57e4e516dc49cb30e1c8913dc
Carl P. Corliss <rabbitt@gmail.com>
2010-10-18 18:34:34 -0400

    anchor the regular expression so that we match on beginning /and/ and ending as well

--- app/models/server_volume.rb
+++ app/models/server_volume.rb
@@ -2,7 +2,7 @@ class ServerVolume < ServerResource
     validates_presence_of :mount_type
     validates_presence_of :mount_point
     validates_uniqueness_of :mount_point, :scope => :resource_bundle_id, :allow_blank => true, :message => 'already used by another resource in this Launch Configuration'
-    validates_format_of :mount_point, :with => %r{/dev/sd[a-z]}, :message => 'device attachment point must match /dev/sd[a-z] and not already be in use'
+    validates_format_of :mount_point, :with => %r{^/dev/sd[a-z]$}, :message => 'device attachment point must match /dev/sd[a-z] and not already be in use'
 #    this wouldn't work for restoring snapshots - disabling until there is a better solution
 #    validates_uniqueness_of :cloud_resource_id, :scope => [ :resource_bundle_id, :type ], :message => 'already mounting in this Launch Configuration'
 end
